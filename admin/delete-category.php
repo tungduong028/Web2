268efd3c9aab193e89d93791f5d058c04e51d535
Quang Tran <quangtranhongquang@gmail.com>
2024-04-05 22:57:37 +0700

    Quang 5-6

---
+++ admin/delete-category.php
@@ -0,0 +1,49 @@
+
+
+
+<?php 
+//include constans file
+include('../admin/config/constants.php');
+//check id and img value is set or not
+  if (isset($_GET['id']) AND isset($_GET['image'])) {
+    # get value and delete
+    $id = $_GET['id'];
+    $image = $_GET['image'];
+    //remove physical img is valaible
+    if ($image != "") {
+        # remove img
+        $path="/images/category/".$image;
+        if (file_exists($path)) {
+            unlink($path);
+        }
+        if ($remove == false) {
+            # set session message
+            $_SESSION['remove'] = "<div class='error'> Faile to remove category </div>";
+            header('location:'.SITEURL.'admin/manage-category.php');
+            die();
+        }
+    }
+    //delete data from database
+
+    //redirect to manage category page
+    $sql = "DELETE FROM category WHERE id=$id";
+    $res = mysqli_query($conn, $sql);
+    //check result of delete
+    if ($res==true) {
+        $_SESSION['delete'] = "<div class='success'>Delete success</div>";
+        header('location:'.SITEURL.'admin/manage-category.php');
+        //die();
+        
+    }
+    else {
+        $_SESSION['delete'] = "<div class='error'>Delete faile</div>";
+        header('location:'.SITEURL.'admin/manage-category.php');
+        //die();
+    }
+  }
+  else {
+    # redirect to maneger category page
+    header('location:'.SITEURL.'admin/manage-category.php');
+    //die();
+  }
+?>
